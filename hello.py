4ab68eab2a37492c015532b677602c5de7c96b70
Ahmadgit config --global user.name Ahmadgit config --global user.name Ahmad <hmdsiyar@#gmail.com>
2026-05-12 09:46:56 -0400

    Update hello.py locally

--- hello.py
+++ hello.py
@@ -1,4 +1,2 @@
-print ("Hello from both GitHub and Local Computer")
-print ("This version combines both edits")
-print ("Local version: Edited Locally")
-print ("GitHub version: edited on GitHub")
+print("Hello from VS Code!")
+print("This version was edited locally")
